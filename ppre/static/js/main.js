ecc6f00ac9114e3e070c92b461611e349564f8ce
Redsnip8 <FCipolone@gmail.com>
2019-09-17 13:02:36 -0400

    JS funtion for Listing pages
    Lisitng fade was added to trim off buttons and to condense the design

--- ppre/static/js/main.js
+++ ppre/static/js/main.js
@@ -4,3 +4,8 @@ document.querySelector('.year').innerHTML = date.getFullYear();
 setTimeout(function() {
   $('#message').fadeOut('slow');
 }, 5000);
+
+$('.card-link').hover(
+  function(){ $(this).parents('div.card').addClass('card-link-active') },
+  function(){ $(this).parents('div.card').removeClass('card-link-active') },
+)
