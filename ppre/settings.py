1fb616d701f9640acf36061b7259ca4ffcb24d50
Redsnip8 <FCipolone@gmail.com>
2019-09-21 17:16:48 -0400

    MySql to PostgreSQL

--- ppre/settings.py
+++ ppre/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'ppre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'prempropdb',
+        'USER': 'frank',
+        'PASSWORD': 'Jur456assic!',
+        'HOST': 'localhost',
     }
 }
 
