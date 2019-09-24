a3c125f93498a7698ec6f1fcc979baeba63b9875
Redsnip8 <FCipolone@gmail.com>
2019-09-23 22:17:59 -0400

    Modles 0.3
    Realtors updated for deafults

--- realtors/models.py
+++ realtors/models.py
@@ -4,10 +4,11 @@ from django.utils import timezone
 class Realtor(models.Model):
     name = models.CharField(max_length=200)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    job_title = models.CharField(default='Realtor', max_length=200)
     description = models.TextField(blank=True)
     phone = models.CharField(max_length=20)
     email = models.CharField(max_length=50)
-    is_mvp = models.BooleanField(default=0)
+    is_mvp = models.BooleanField(default=False)
     hire_date = models.DateTimeField(default=timezone.now, blank=True)
 
     def __str__(self):
