917b5f09bab37d4817c52970f85b6b569c02c418
Redsnip8 <FCipolone@gmail.com>
2019-10-01 14:24:43 -0400

    Update 0.7
    Client wanted ability to clear search field.

--- ppre/static/js/main.js
+++ ppre/static/js/main.js
@@ -9,3 +9,20 @@ $('.card-link').hover(
   function(){ $(this).parents('div.card').addClass('card-link-active') },
   function(){ $(this).parents('div.card').removeClass('card-link-active') },
 )
+
+function bootstrapClearButton() {
+  $('.position-relative :input').on('keydown focus', function() {
+    if ($(this).val().length > 0) {
+      $(this).nextAll('.form-clear').removeClass('d-none');
+    }
+  }).on('keydown keyup blur', function() {
+    if ($(this).val().length === 0) {
+      $(this).nextAll('.form-clear').addClass('d-none');
+    }
+  });
+  $('.form-clear').on('click', function() {
+    $(this).addClass('d-none').prevAll(':input').val('');
+  });
+}
+
+bootstrapClearButton();
