2fcd15b8769a8cd1e3bd79a8d8d71479ebb28622
Redsnip8 <FCipolone@gmail.com>
2019-10-01 12:42:22 -0400

    update 0.5
    views updated for search on index

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
+from listings.models import Listing
+from realtors.models import Realtor
+from listings.choices import bedroom_choices, price_choices, neighborhood_choice
+
 
 def index(request):
-  return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by(
+        '-list_date').filter(is_published=True)[:3]
+    context = {
+        'listings': listings,
+        'neighborhood_choice': neighborhood_choice,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+    }
+    return render(request, 'pages/index.html', context)
+
 
 def about(request):
-  return render(request, 'pages/about.html')
+    # get all realtors
+    realtors = Realtor.objects.order_by('-job_title')
+
+    context = {
+        'realtors': realtors
+    }
+
+    return render(request, 'pages/about.html', context)
 
 def sell(request):
-  return render(request, 'pages/sell.html')
+  # get MVP
+  mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+  context = {
+    'mvp_realtors': mvp_realtors
+  }
+
+  return render(request, 'pages/sell.html', context)
